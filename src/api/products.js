24a57d9e74ec023c5b096881cc92229b95c4d565
MinhQuang-dev <phanquang704@gmail.com>
2025-04-19 06:46:39 +0700

    Update route

--- src/api/products.js
+++ src/api/products.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const API_URL = `${import.meta.env.VITE_API_URL}/products`
+console.log(`${import.meta.env.VITE_API_URL}/products`);
+
 
 
 // Hàm để lấy danh sách sản phẩm
--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',  // Địa chỉ backend
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),  // Thay đổi đường dẫn URL cho API
-      },
+
     },
   },
 })
